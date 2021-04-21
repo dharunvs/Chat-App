b2ace94b5e764681c3056c4437315912815ce3b5
Dharun Sivakumar <72387676+dharunvs@users.noreply.github.com>
2021-04-21 12:03:38 +0530

    Update firebase.js

--- app/src/service/firebase.js
+++ app/src/service/firebase.js
@@ -5,14 +5,6 @@ import "firebase/firestore";
 
 try {
   firebase.initializeApp({
-    // apiKey: "AIzaSyCon70IoW9avHRkBWmDTTXR_C9M2UIJ30Q",
-    // authDomain: "chat-app-9e116.firebaseapp.com",
-    // projectId: "chat-app-9e116",
-    // storageBucket: "chat-app-9e116.appspot.com",
-    // messagingSenderId: "758679960162",
-    // appId: "1:758679960162:web:e382ba0a16ffebf7212f3b",
-    // measurementId: "G-6VZF6PMDKD",
-
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
